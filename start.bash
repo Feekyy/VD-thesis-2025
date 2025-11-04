d84f6155ce4149b3b3af4129ff92bc9a45eee4fe
Feekyy <vitkolczi.daniel@gmail.com>
2025-11-04 20:41:18 +0100

    Some minor changes

--- start.bash
+++ start.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-DOCKER_IMAGE="ros-foxy-tello:latest"
+DOCKER_IMAGE="ros2-foxy-tello:latest"
 CONTAINER_NAME="ros-foxy-tello"
 
-echo "-----------------------------------"\n
+echo "-----------------------------------"
 echo " Tello Drone Launcher Script"
 echo "-----------------------------------"
 echo "Commands:"
+echo "  build	- Start building the docker"
 echo "  start   - Foxy Docker starter"
 echo "  start2  - Secondary Docker starter"
 echo "  gazebo  - Gazebo simulation starter"
@@ -17,28 +18,39 @@ echo "-----------------------------------"
 read -p "Write here: " CMD
 
 case "$CMD" in
+	"build")
+		echo "Building the docker!"
+		cd Docker
+		sudo docker build -t ros2-foxy-tello .
+	;;
 
 	"start")
     	echo "Starting docker..."
-	docker stop $CONTAINER_NAME
-	docker rm $CONTAINER_NAME
-	xhost +local:root
-	docker run -it --rm --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/tello_ros_ws:/root/tello_ros_ws -v /dev/dri:/dev/dri --device /dev/dri --name $CONTAINER_NAME $DOCKER_IMAGE bash
+    	docker stop $CONTAINER_NAME 2>/dev/null
+    	docker rm $CONTAINER_NAME 2>/dev/null
+    	xhost +local:docker
+		sudo docker run -it --net=host --env="DISPLAY=$DISPLAY" --env="QT_X11_NO_MITSHM=1" --env="XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --volume="$HOME/DockerFiles:/root/DockerFiles" --name ros2_foxy_tello ros2-foxy-tello:latest
+
 	;;
 
 	"start2")
-	echo "Starting another docker..."
-	docker exec -it $CONTAINER_NAME $DOCKER_IMAGE bash
+    	echo "Starting another docker shell..."
+    	sudo docker exec -it $CONTAINER_NAME bash
 	;;
 
 	"gazebo")
-	read -p "Give me the world name: " CMD
-	CMD += ".world"
-	echo "Starting the gazeboo simulation..."
-	gazebo /root/tello_ros_ws/sim/worlds/$CMD --verbose
+    	read -p "Give me the world name: " WORLD
+    	WORLD="${WORLD}.world"
+    	echo "Starting the Gazebo simulation with world: $WORLD"
+    	sudo docker exec -it $CONTAINER_NAME bash -c "gazebo /root/tello_ros_ws/sim/worlds/${WORLD} --verbose"
 	;;
 
-	*)
-    	echo "Wrong command! Exiting!"
-    	exit 1
+	"exit")
+    	exit 0
+    	;;
+
+    	*)
+    	echo "Wrong command"
+    	exit 0
+    	;;
 esac
